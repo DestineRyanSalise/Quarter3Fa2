bda31d73a842fe84d37feef3f205d86ac521f35c
DestineRyanSalise <destineryan.salise@smc.pshs.edu.ph>
2025-02-10 20:02:47 +0800

    Update Fa2.js

--- Fa2.js
+++ Fa2.js
@@ -1,31 +1,35 @@
-function add() {
-  var num1 = document.getElementById("myText").value;
-  var num2 = document.getElementById("myText2").value;
+function addition() {
+  var num1 = document.getElementById("number1").value;
+  var num2 = document.getElementById("number2").value;
   var sum = parseInt(num1) + parseInt(num2);
-  document.getElementById("result").innerHTML = "The sum of " + num1 + " and " + num2 + " is " + sum;
+  document.getElementById("output").innerHTML = "The sum of " + num1 + " and " + num2 + " is " + sum;
 }
-function sub() {
-    var num1 = document.getElementById("myText").value;
-    var num2 = document.getElementById("myText2").value;
-    var diff = parseInt(num1) - parseInt(num2);
-    document.getElementById("result").innerHTML = "The difference of " + num1 + " and " + num2 + " is " + diff;
-  }
-function mul() {
-    var num1 = document.getElementById("myText").value;
-    var num2 = document.getElementById("myText2").value;
-    var prod = parseInt(num1) * parseInt(num2);
-    document.getElementById("result").innerHTML = "The product of " + num1 + " and " + num2 + " is " + prod;
-  }
-function div() {
-    var num1 = document.getElementById("myText").value;
-    var num2 = document.getElementById("myText2").value;
-    var quot = parseInt(num1) / parseInt(num2);
-    quot = quot.toFixed(2);
-    document.getElementById("result").innerHTML = "The quotient of " + num1 + " and " + num2 + " is " + quot;
-  }
-function mod() {
-    var num1 = document.getElementById("myText").value;
-    var num2 = document.getElementById("myText2").value;
-    var mod = parseInt(num1) % parseInt(num2);
-    document.getElementById("result").innerHTML = "The remainder of " + num1 + " and " + num2 + " is " + mod;
-  }
+
+function subtraction() {
+  var num1 = document.getElementById("number1").value;
+  var num2 = document.getElementById("number2").value;
+  var diff = parseInt(num1) - parseInt(num2);
+  document.getElementById("output").innerHTML = "The difference of " + num1 + " and " + num2 + " is " + diff;
+}
+
+function multiplication() {
+  var num1 = document.getElementById("number1").value;
+  var num2 = document.getElementById("number2").value;
+  var prod = parseInt(num1) * parseInt(num2);
+  document.getElementById("output").innerHTML = "The product of " + num1 + " and " + num2 + " is " + prod;
+}
+
+function division() {
+  var num1 = document.getElementById("number1").value;
+  var num2 = document.getElementById("number2").value;
+  var quot = parseInt(num1) / parseInt(num2);
+  quot = quot.toFixed(2);
+  document.getElementById("output").innerHTML = "The quotient of " + num1 + " and " + num2 + " is " + quot;
+}
+
+function modulus() {
+  var num1 = document.getElementById("number1").value;
+  var num2 = document.getElementById("number2").value;
+  var mod = parseInt(num1) % parseInt(num2);
+  document.getElementById("output").innerHTML = "The remainder of " + num1 + " and " + num2 + " is " + mod;
+}
